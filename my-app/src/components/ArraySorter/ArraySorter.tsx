f33c5d459be287c8482491e0d622111cf4f5f9ca
Mountain Cat <tomasz.banas123@gmail.com>
2023-03-16 18:39:08 +0100

    🔥🔥🔥 POWERFUL COMMIT! 🔥🔥🔥

---
+++ my-app/src/components/ArraySorter/ArraySorter.tsx
@@ -0,0 +1,49 @@
+﻿import React, {useEffect, useState} from "react";
+import {bubbleSort} from "../../utilities/bubbleSort";
+
+import './ArraySorter.css'
+
+export function ArraySorter() {
+    const [input, setInput] = useState<string>("")
+    const [output, setOutput] = useState<string>("")
+
+    function onChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setInput(event.currentTarget.value);
+    }
+
+    function onClick(event: React.MouseEvent) {
+        Sort();
+    }
+
+    function Sort() {
+        let inputFixed: string = input
+            .replaceAll(' ', '')
+            .replace(/[a-zA-Z]/g, '');
+
+        setInput(inputFixed);
+
+        let inputArray = inputFixed
+            .split(',')
+            .map(Number)
+
+        let sortedArray = bubbleSort(inputArray);
+
+        setOutput(sortedArray.toString());
+    }
+
+    return (
+        <div className={'sorter'}>
+            <div className={"grid-panel"}>
+                <textarea className={'input-field'} value={input} onChange={onChange}/>
+            </div>
+            <div className={"grid-panel button-panel"}>
+                <button className={'sort-button'} onClick={onClick}>
+                    Sort!
+                </button>
+            </div>
+            <div className={"grid-panel"}>
+                <textarea className={'input-field'} value={output}/>
+            </div>
+        </div>
+    )
+}
---
+++ my-app/src/utilities/bubbleSort.ts
@@ -0,0 +1,16 @@
+﻿export function bubbleSort<T>(arr: T[]): T[] {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap arr[j] and arr[j+1]
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
